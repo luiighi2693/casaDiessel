56e30ecf31324af56197c0002f419ba1959bd697
luiighi2693 <luiscarvajal2693@gmail.com>
2017-02-19 23:54:45 -0400

    diseño de primeras vistas

--- main.js
+++ main.js
@@ -3,23 +3,6 @@ function main() {
 
 (function () {
    'use strict';
-
-	// Hide .navbar first
-	// $(".navbar").hide();
-	
-	// Fade in .navbar
-	// $(function () {
-	// 	$(window).scroll(function () {
-     //        // set distance user needs to scroll before we fadeIn navbar
-	// 		if ($(this).scrollTop() > 200) {
-	// 			$('.navbar').fadeIn();
-	// 		} else {
-	// 			$('.navbar').fadeOut();
-	// 		}
-	// 	});
-    //
-	//
-	// });
 	
 	// Preloader */
 	  	$(window).load(function() {
@@ -45,31 +28,6 @@ function main() {
           }
         }
       });
-
-    // Show Menu on Book
-    // $(window).bind('scroll', function() {
-    //     var navHeight = $(window).height() - 100;
-    //     if ($(window).scrollTop() > navHeight) {
-    //         $('.navbar-default').addClass('on');
-    //     } else {
-    //         $('.navbar-default').removeClass('on');
-    //     }
-    // });
-
-    // $('body').scrollspy({
-    //     target: '.navbar-default',
-    //     offset: 80
-    // })
-
-  	// $(document).ready(function() {
-  	//     $("#testimonial").owlCarousel({
-       //  navigation : false, // Show next and prev buttons
-       //  slideSpeed : 300,
-       //  paginationSpeed : 400,
-       //  singleItem:true
-       //  });
-      //
-  	// });
 
   	// Portfolio Isotope Filter
     $(window).load(function() {
