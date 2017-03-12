b107424b0a1db08925923ec7032265e1954f9829
luis <luiscarvajal2693@gmail.com>
2017-03-12 18:14:19 -0400

    refactorizacion de consultas backend

--- admin/util.php
+++ admin/util.php
@@ -16,6 +16,26 @@ if($_POST['method']=='findByUser'){
     findByUser();
 }
 
+if($_POST['method']=='getAllProducts'){
+    getAllProducts();
+}
+
+if($_POST['method']=='findByProduct'){
+    findByProduct();
+}
+
+if($_POST['method']=='getAllRoles'){
+    getAllRoles();
+}
+
+if($_POST['method']=='getAllMaquinarias'){
+    getAllMaquinarias();
+}
+
+if($_POST['method']=='getAllTipos'){
+    getAllTipos();
+}
+
 function getAllUsers(){
     $query = 'SELECT id, nombre, cedula FROM usuario';
     executeQuery($query);
@@ -25,6 +45,35 @@ function findByUser(){
     $data = "'%".$_POST['search']."%'";
     $query = 'SELECT id, nombre, cedula FROM usuario WHERE nombre LIKE '.$data.' UNION
         SELECT id, nombre, cedula FROM usuario WHERE cedula LIKE '.$data;
+    executeQuery($query);
+}
+
+function getAllProducts(){
+    $query = 'SELECT id, nombre, codigo, marca, referencia FROM producto';
+    executeQuery($query);
+}
+
+function findByProduct(){
+    $data = "'%".$_POST['search']."%'";
+    $query = 'SELECT id, nombre, codigo, marca, referencia FROM producto WHERE nombre LIKE '.$data.' UNION
+        SELECT id, nombre, codigo, marca, referencia FROM producto WHERE codigo LIKE '.$data.' UNION
+        SELECT id, nombre, codigo, marca, referencia FROM producto WHERE marca LIKE '.$data.' UNION
+        SELECT id, nombre, codigo, marca, referencia FROM producto WHERE referencia LIKE '.$data;
+    executeQuery($query);
+}
+
+function getAllRoles(){
+    $query = 'SELECT * FROM rol';
+    executeQuery($query);
+}
+
+function getAllMaquinarias(){
+    $query = 'SELECT * FROM maquinaria';
+    executeQuery($query);
+}
+
+function getAllTipos(){
+    $query = 'SELECT * FROM tipo';
     executeQuery($query);
 }
 
