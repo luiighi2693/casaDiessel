b3cf2bc3418c7755305f8a870cb593ab6e81ba41
luiighi2693 <luiscarvajal2693@gmail.com>
2017-03-23 17:51:41 -0400

    nuevo script

--- casadiessel.sql
+++ casadiessel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-03-2017 a las 19:47:27
--- Versión del servidor: 5.7.9
--- Versión de PHP: 5.6.16
+-- Tiempo de generación: 23-03-2017 a las 22:51:16
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,17 +23,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `foto`
+-- Estructura de tabla para la tabla `fotos`
 --
 
-DROP TABLE IF EXISTS `foto`;
-CREATE TABLE IF NOT EXISTS `foto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fotos` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(150) DEFAULT NULL,
   `fecha` varchar(50) DEFAULT NULL,
-  `idProducto` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `foto_id_uindex` (`id`)
+  `idProducto` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -42,13 +39,10 @@ CREATE TABLE IF NOT EXISTS `foto` (
 -- Estructura de tabla para la tabla `maquinaria`
 --
 
-DROP TABLE IF EXISTS `maquinaria`;
-CREATE TABLE IF NOT EXISTS `maquinaria` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `maquinaria_id_uindex` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `maquinaria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `maquinaria`
@@ -66,19 +60,16 @@ INSERT INTO `maquinaria` (`id`, `nombre`) VALUES
 -- Estructura de tabla para la tabla `producto`
 --
 
-DROP TABLE IF EXISTS `producto`;
-CREATE TABLE IF NOT EXISTS `producto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `codigo` varchar(50) DEFAULT NULL,
   `descripcion` varchar(500) DEFAULT NULL,
   `marca` varchar(50) DEFAULT NULL,
   `idTipo` int(11) DEFAULT NULL,
   `caracteristicas` varchar(100) DEFAULT NULL,
-  `referencia` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `producto_id_uindex` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `referencia` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `producto`
@@ -94,13 +85,10 @@ INSERT INTO `producto` (`id`, `nombre`, `codigo`, `descripcion`, `marca`, `idTip
 -- Estructura de tabla para la tabla `producto_has_maquinaria`
 --
 
-DROP TABLE IF EXISTS `producto_has_maquinaria`;
-CREATE TABLE IF NOT EXISTS `producto_has_maquinaria` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `producto_has_maquinaria` (
+  `id` int(11) NOT NULL,
   `idProducto` int(11) DEFAULT NULL,
-  `idMaquinaria` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `producto_has_maquinaria_id_uindex` (`id`)
+  `idMaquinaria` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -109,13 +97,10 @@ CREATE TABLE IF NOT EXISTS `producto_has_maquinaria` (
 -- Estructura de tabla para la tabla `rol`
 --
 
-DROP TABLE IF EXISTS `rol`;
-CREATE TABLE IF NOT EXISTS `rol` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `codigo` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `rol_id_uindex` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+CREATE TABLE `rol` (
+  `id` int(11) NOT NULL,
+  `codigo` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `rol`
@@ -132,13 +117,10 @@ INSERT INTO `rol` (`id`, `codigo`) VALUES
 -- Estructura de tabla para la tabla `tipo`
 --
 
-DROP TABLE IF EXISTS `tipo`;
-CREATE TABLE IF NOT EXISTS `tipo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `tipo_id_uindex` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+CREATE TABLE `tipo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tipo`
@@ -155,17 +137,13 @@ INSERT INTO `tipo` (`id`, `nombre`) VALUES
 -- Estructura de tabla para la tabla `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `cedula` int(11) DEFAULT NULL,
   `clave` varchar(50) DEFAULT NULL,
-  `idRol` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `usuario_id_uindex` (`id`),
-  KEY `usuario_rol_id_fk` (`idRol`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `idRol` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -176,6 +154,94 @@ INSERT INTO `usuario` (`id`, `nombre`, `cedula`, `clave`, `idRol`) VALUES
 (2, 'juan123', 12345678, '12345', 2),
 (7, 'polaco', 12345678, '12345', 1);
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `foto_id_uindex` (`id`,`idProducto`);
+
+--
+-- Indices de la tabla `maquinaria`
+--
+ALTER TABLE `maquinaria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `maquinaria_id_uindex` (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `producto_id_uindex` (`id`);
+
+--
+-- Indices de la tabla `producto_has_maquinaria`
+--
+ALTER TABLE `producto_has_maquinaria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `producto_has_maquinaria_id_uindex` (`id`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `rol_id_uindex` (`id`);
+
+--
+-- Indices de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tipo_id_uindex` (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario_id_uindex` (`id`),
+  ADD KEY `usuario_rol_id_fk` (`idRol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `maquinaria`
+--
+ALTER TABLE `maquinaria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `producto_has_maquinaria`
+--
+ALTER TABLE `producto_has_maquinaria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
